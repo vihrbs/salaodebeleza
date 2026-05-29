cb57604b96e77eb873a7f6c5afc8553bbfdc07fe
vihrbs <ty2pr6h4jb@privaterelay.appleid.com>
2026-05-29 17:42:13 -0300

    server.js

--- server.js
+++ server.js
@@ -6,17 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// rota raiz (IMPORTANTE)
 app.get('/', (req, res) => {
   res.send('API rodando');
 });
 
-// rota health
 app.get('/health', (req, res) => {
   res.send('ok');
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log('Servidor rodando na porta ' + PORT);
